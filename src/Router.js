2dfa6eac1401dc9684067ff2ef4f7e4c0ab33309
sinthan1490 <sinthan.siri@bumail.net>
2022-05-29 11:49:20 +0700

    Exercise 06-01 for 1610701110

--- src/Router.js
+++ src/Router.js
@@ -1,19 +1,16 @@
-import App from './App.js';
-import Profile from './Profile.js';
-import Minigame from './Minigame.js';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import Steamchat from './Steamchat.js';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-//index.js --> Router.js --> App.js
+import App from './App.js'
+import Profile from './Profile.js'
+import Video from './Video.js'
 
 function Router(){
     return (
         <BrowserRouter>
-        <Routes>
-            <Route path="/" element={ <App /> } />
-            <Route path="/profile" element={ <Profile /> } />
-            <Route path="/minigame" element={ <Minigame /> } />
-            <Route path="/steamchat" element={ <Steamchat /> } />
+            <Routes>
+                <Route path="/" element={ <App/>} />
+                <Route path="/profile" element={ <Profile/> } />
+                <Route path="/Video" element={ <Video/> } />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/Video.js
@@ -0,0 +1,12 @@
+import './Video.css';
+
+function Video(){
+    return (
+    <div>
+        <div className="Video">Recommend video</div>
+        <div className="More">More video</div>
+    </div>
+    )
+}
+
+export default Video;
